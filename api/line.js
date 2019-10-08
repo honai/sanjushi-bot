f0e897df0e41797f86ffde5df2465746cb36f0f2
honai <29170100+honai@users.noreply.github.com>
2019-10-08 21:48:46 +0900

    rebase

--- api/line.js
+++ api/line.js
@@ -26,14 +26,14 @@ async function handleEvent(event) {
       }
       break
     case message.actionDef.SETNAME:
-      const reply = await action.setName(event)
-      if (reply) {
+      const reply2 = await action.setName(event)
+      if (reply2) {
         return client.replyMessage(event.replyToken, reply2)
       }
       break
     case message.actionDef.COUNT:
-      const reply = await action.coutFromMessage(event, client)
-      if (reply) {
+      const reply3 = await action.coutFromMessage(event, client)
+      if (reply3) {
         return client.replyMessage(event.replyToken, reply3)
       }
       break
